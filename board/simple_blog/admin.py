ac9699b5e0a74956c9ef48cad0e85f052d7e3edf
nikitasmall <nikitasosnov@yahoo.com>
2016-03-15 14:35:20 +0200

    implement startup script and install script

--- board/simple_blog/admin.py
+++ board/simple_blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Subcategory, Topic
+from .models import Category, Subcategory, Topic, Message
 
 admin.site.register(Category)
 admin.site.register(Subcategory)
 admin.site.register(Topic)
+admin.site.register(Message)
---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+# use this script only once to get the preparations
+
+
+# run migrations, creates superuser and fill the DB
+venv/bin/python board/manage.py migrate
+venv/bin/python board/manage.py createsuperuser
