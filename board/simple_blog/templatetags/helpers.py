5450ae6123b26533212a725f8b6f5f7847a7581e
nikitasmall <nikitasosnov@yahoo.com>
2016-03-15 13:54:04 +0200

    implement image checks

--- board/simple_blog/templatetags/helpers.py
+++ board/simple_blog/templatetags/helpers.py
@@ -1,6 +1,8 @@
 from django import template
 from simple_blog.models import Category
 
+import imghdr
+
 register = template.Library()
 
 
@@ -8,3 +10,15 @@ register = template.Library()
 def category_nav():
     categories = Category.objects.order_by('-pub_date')
     return {'categories': categories}
+
+
+@register.inclusion_tag('includes/image_check.html')
+def image_check(image):
+    if image:
+        img_path = image.url
+        file_type = imghdr.what(image.path)
+
+        if file_type == 'png' or file_type == 'jpeg' or file_type == 'gif' or file_type == 'jpg':
+            return {'image': img_path}
+
+    return {'image': None}
